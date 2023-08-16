9273022832098851f70193a9bb33b5b9add24abd
Manish Nath <manishnath2001@gmail.com>
2023-08-16 11:10:48 +0530

    Initial commit

---
+++ OnlineHotel.Utility/WebsiteRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineHotel.Utility
+{
+    public static class WebsiteRoles
+    {
+        public const string Hotel_Admin = "Admin";
+        public const string Hotel_Customer = "Customer";
+    }
+}
