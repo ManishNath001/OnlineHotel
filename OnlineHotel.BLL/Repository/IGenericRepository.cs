9ca0bfc6acb02a884d6672e822770144634a7a3c
Manish Nath <manishnath2001@gmail.com>
2023-08-18 12:06:07 +0530

    Generic Repo

--- OnlineHotel.BLL/Repository/IGenericRepository.cs
+++ OnlineHotel.BLL/Repository/IGenericRepository.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineHotel.BLL.Repository
 {
-    internal interface IGenericRepository<T> : IDisposable
+    public interface IGenericRepository<T> : IDisposable
     {
         IEnumerable<T> GetAll(
-            
-            );
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null, string includeProperties="");
         T GetById(object id);
         Task<T> GetByIdAsync(object id);
         void Add(T entity);
